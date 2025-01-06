220bb4d9c17873c93d232131f0fdb0995d533027
Vitaliy <kapralsv@yandex.ru>
2025-01-06 18:44:03 +0300

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['89.169.171.227', '127.0.0.1', 'localhost', 'ksv-taski.ru', 'www.ksv-taski.ru']
+ALLOWED_HOSTS = ['89.169.171.227', '127.0.0.1', 'localhost', 'ksv-taski.ru']
 
 
 # Application definition
